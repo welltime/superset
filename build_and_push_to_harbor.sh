69564ddbfefddba56b9c9653ebb17435149166ea
alexander_perov <perov@mcn.ru>
2022-01-25 19:59:34 +0300

    fix build_and_push_to_harbor.sh

--- build_and_push_to_harbor.sh
+++ build_and_push_to_harbor.sh
@@ -2,5 +2,9 @@ THIS=`readlink -f "${BASH_SOURCE[0]}"`
 DIR=`dirname "${THIS}"`
 cd $DIR
 
-docker build -t k8s-prod1-harbor.mcn.ru/superset/superset:latest . &&
-	  docker push k8s-prod1-harbor.mcn.ru/superset/superset:latest
+sha=$(git rev-parse HEAD)
+docker rmi k8s-prod1-harbor.mcn.ru/superset/superset:latest &&
+docker build -t k8s-prod1-harbor.mcn.ru/superset/superset:$sha \
+    -t k8s-prod1-harbor.mcn.ru/superset/superset:latest . &&
+    docker push k8s-prod1-harbor.mcn.ru/superset/superset:$sha &&
+    docker push k8s-prod1-harbor.mcn.ru/superset/superset:latest
